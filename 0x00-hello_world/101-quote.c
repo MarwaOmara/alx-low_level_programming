fa0898feb26d3f3ce04677e2ff944450b1049874
MarwaOmara <beboemara2020@gmail.com>
2023-07-13 10:04:55 -0700

    My commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - main block
- * return: 1 (unsuccessful)
- * write function (int fd, const void, buf.size_t count)
+ * main - Entry point
+ * Return:1
  */
-
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, quo, 59);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
