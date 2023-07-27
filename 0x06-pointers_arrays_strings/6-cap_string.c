62fcb6177db2b0e14da42b54bfd015e3b269246f
MarwaOmara <beboemara2020@gmail.com>
2023-07-27 11:37:45 -0700

    m

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,16 +1,43 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * isLower - determines whether ascii is lowercase
+ * @c: Character
+ * Return: 1 if true, 0 if false
+*/
+
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * isDelimiter - determines whether ascii is a delimiter
+ * @c: Character
+ * Return: 1 if true, 0 if false
+*/
+
+int isDelimiter(char c)
+{
+	int i;
+	char delimiter[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+	return (0);
+}
 
 /**
  * cap_string - capitalizes all words of a string
  * @s: input string
  * Return: string with capitalized words
- *
- *
- */
+*/
+
 char *cap_string(char *s)
 {
 	char *ptr = s;
-
 	int foundDelimit = 1;
 
 	while (*s)
@@ -20,7 +47,7 @@ char *cap_string(char *s)
 		else if (isLower(*s) && foundDelimit)
 		{
 			*s -= 32;
-		foundDelimit = 0;
+			foundDelimit = 0;
 		}
 		else
 			foundDelimit = 0;
