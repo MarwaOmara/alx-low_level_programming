7bca55bac758ac2fcb2236a84695ab7f056f4dcb
MarwaOmara <beboemara2020@gmail.com>
2023-08-04 17:06:00 -0700

    agrv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,14 +12,14 @@ int main(int argc, char *argv[])
 {
 	if (argc == 2)
 	{
-		int i, lowercents = 0, money = atoi(argv[1]);
+		int i, leastcents = 0, money = atoi(argv[1]);
 		int cents[] = (25, 10, 5, 2, 1);
 
 		for (i = 0; i < 5; i++)
 		{
 			if (money >= cents[i])
 			{
-				lowercents += money / cents[i];
+				leastcents += money / cents[i];
 				money = money % cents[i];
 				if (money % cents[i] == 0)
 				{
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 				}
 			}
 		}
-		printf("%d\n", lowercents);
+		printf("%d\n", leastcents);
 	}
 	else
 	{
