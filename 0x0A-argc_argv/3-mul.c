97d6e0ecf4bb9e8c9defddc1150478c9c010dab6
MarwaOmara <beboemara2020@gmail.com>
2023-08-04 17:28:49 -0700

    argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,22 +4,23 @@
  * main - prints sum of 2 number
  * @argc: number of arguments
  * @argv: poiner to an array of arguments
+ * @num1: first number
+ * @num2: second number
+ * @product: sum of the two numbers
  * Return: 0 Success, non-zero-fail
 */
 
-int main(int argc, char *argv[])
-{
-	int sum;
-
-	if (argc == 3)
-	{
-		sum = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", sum);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
-	return (0);
+int main(int argc, char *argv[]) {
+if (argc != 3) {
+printf("Error\n");
+return 1;
 }
+
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2]);
+int product = num1 * num2;
+
+printf("%d\n", product);
+return 0;
+}
+
